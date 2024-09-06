7210171bbb3ac9aa3dce88b6448098b99bfca72a
Jeff Palmer <jeff.m.palmer42@gmail.com>
2024-09-06 19:27:29 -0400

    Testing DAO

--- src/test/java/ca/toadapp/api_main/ApiMainApplicationTests.java
+++ src/test/java/ca/toadapp/api_main/ApiMainApplicationTests.java
@@ -1,6 +1,7 @@
 package ca.toadapp.api_main;
 
 import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -9,60 +10,100 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ca.toadapp.common.data.entity.DaoAgent;
+import ca.toadapp.common.data.entity.DaoAgentLocation;
+import ca.toadapp.common.data.entity.DaoAgentNotification;
 import ca.toadapp.common.data.entity.DaoDelCo;
+import ca.toadapp.common.data.entity.DaoDelCoBranding;
 import ca.toadapp.common.data.entity.DaoDelCoHour;
+import ca.toadapp.common.data.entity.DaoDelCoLocation;
 import ca.toadapp.common.data.entity.DaoDeliveryType;
-import ca.toadapp.common.data.repository.RepoDelCo;
-import ca.toadapp.common.data.repository.RepoDelCoHour;
-import ca.toadapp.common.data.repository.RepoDeliveryTypes;
+import ca.toadapp.common.data.enumeration.AgentRoles;
+import ca.toadapp.common.data.enumeration.NotificationTypes;
+import ca.toadapp.common.service.ServiceAgent;
+import ca.toadapp.common.service.ServiceDelCo;
+import ca.toadapp.common.service.ServiceDeliveryTypes;
 
 @SpringBootTest
 class ApiMainApplicationTests {
+	@Autowired
+	private ServiceDeliveryTypes deliveryTypes;
+
+	@Autowired
+	private ServiceDelCo deliveryCompany;
+
+	@Autowired
+	private ServiceAgent serviceAgent;
 
 	@Test
 	void contextLoads() {
-		popDB();
+		var delTypes = popDeliveryTypes();
+		var delCo = popDeliveryCompany(delTypes);
+		var agent = popAgent(delCo);
+
+		deliveryCompany.updateDispatcher(delCo.getId(), agent.getId());
 	}
 
-	@Autowired
-	private RepoDeliveryTypes deliveryTypes;
-
-	@Autowired
-	private RepoDelCo deliveryCompany;
-
-	@Autowired
-	private RepoDelCoHour deliveryCompanyHour;
-
-	void popDB() {
+	Collection<DaoDeliveryType> popDeliveryTypes() {
 		final var delTypes = new ArrayList<DaoDeliveryType>();
 		delTypes.add(new DaoDeliveryType("Standard Food Delivery", 7.00));
 		delTypes.add(new DaoDeliveryType("Grocery Delivery", 10.00));
 		delTypes.add(new DaoDeliveryType("Merchandice Delivery", 10.00));
 		delTypes.add(new DaoDeliveryType("Catering Delivery", 15.00));
-		final var addedDelTypes = deliveryTypes.saveAll(delTypes);
+		return deliveryTypes.saveAll(delTypes);
+	}
 
+	DaoDelCo popDeliveryCompany(Collection<DaoDeliveryType> delTypes) {
 		final var delCo = new DaoDelCo();
 		delCo.setName("Stork Delivery");
 		delCo.setCompanyCode("stork");
 		delCo.setPhone("5558675309");
 		delCo.setAddress("St.Ratford");
 		delCo.setHasDeliveryOnDemand(true);
-		delCo.setDeliveryTypes(addedDelTypes.stream().map(c -> c.getId()).toList());
+		delCo.setDeliveryTypes(delTypes.stream().map(c -> c.getId()).toList());
 
-		final var addedDelCo = deliveryCompany.save(delCo);
+		final var hours = new ArrayList<DaoDelCoHour>();
+		hours.add(new DaoDelCoHour(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.TUESDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.WEDNESDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.THURSDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.FRIDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.SATURDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
+		hours.add(new DaoDelCoHour(DayOfWeek.SUNDAY, LocalTime.of(11, 0), LocalTime.of(19, 0)));
+		delCo.setHours(hours);
 
-		final var delCoHours = new ArrayList<DaoDelCoHour>();
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.MONDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.TUESDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.WEDNESDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.THURSDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.FRIDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.SATURDAY, LocalTime.of(9, 0), LocalTime.of(22, 0)));
-		delCoHours.add(new DaoDelCoHour(DayOfWeek.SUNDAY, LocalTime.of(11, 0), LocalTime.of(19, 0)));
+		final var locaitons = new ArrayList<DaoDelCoLocation>();
+		locaitons.add(new DaoDelCoLocation(43.369986691835855, -80.98213896348206, 5, 5.00, 1.00, 1.50));
+		delCo.setServiceAreas(locaitons);
 
-		final var savedHours = deliveryCompanyHour.saveAll(delCoHours);
-		
-		
+		final var branding = new DaoDelCoBranding("fontName", "textColor", "fgColour", "bgColour", "http://logo",
+				"http://web", "http://menu");
+		delCo.setBranding(branding);
+
+		return deliveryCompany.save(delCo, true, true, true);
+
+	}
+
+	private DaoAgent popAgent(DaoDelCo delCo) {
+		final var roles = new ArrayList<AgentRoles>();
+		roles.add(AgentRoles.admin);
+		roles.add(AgentRoles.dispatch);
+		roles.add(AgentRoles.driver);
+		var agent = new DaoAgent("god@stork", roles, LocalDateTime.now(), "God", "Heaven", "558675309", delCo,
+				delCo.getId(), false, false, false);
+
+		agent = serviceAgent.save(agent);
+
+		final var location = new DaoAgentLocation(null, agent.getId(), 43.369986691835855, -80.98213896348206);
+		serviceAgent.setLocation(location);
+
+		final var notificationOptions = new ArrayList<DaoAgentNotification>();
+		notificationOptions
+				.add(new DaoAgentNotification(null, agent.getId(), NotificationTypes.sms, "5558675309", true));
+		notificationOptions
+				.add(new DaoAgentNotification(null, agent.getId(), NotificationTypes.gcm, "firebaseGCM", false));
+		serviceAgent.setNotificationConfig(notificationOptions);
+		return agent;
 	}
 
 }
