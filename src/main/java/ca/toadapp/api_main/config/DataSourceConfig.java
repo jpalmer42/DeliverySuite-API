59a4538e3c4032766aa07b1a910c17df970ce1d8
Jeff Palmer <jeff.m.palmer42@gmail.com>
2024-09-15 16:51:12 -0400

    Better Datasource and GeoAPI

--- src/main/java/ca/toadapp/api_main/config/DataSourceConfig.java
+++ src/main/java/ca/toadapp/api_main/config/DataSourceConfig.java
@@ -2,7 +2,7 @@ package ca.toadapp.api_main.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,24 +12,28 @@ import lombok.extern.java.Log;
 @Log
 @Configuration
 public class DataSourceConfig {
-
-	@Autowired
-	private VaultConfig vaultConfig;
+	@Value("${db.driver}")
+	private String driver;
+	
+	@Value("${db.url}")
+	private String url;
+	
+	@Value("${db.user}")
+	private String user;
+	
+	@Value("${db.password}")
+	private String password;
 
 	@Bean
 	public DataSource getDataSource() {
-		final var driver = vaultConfig.getDriver();
-		final var url = vaultConfig.getUrl();
-		final var user = vaultConfig.getUser();
-		final var pwd = vaultConfig.getPassword();
-
+		
 		log.info( String.format( "dbURL: %s, dbUser: %s", url, user ) );
 		 
 		final var dataSourceBuilder = DataSourceBuilder.create();
 		dataSourceBuilder.driverClassName( driver );
 		dataSourceBuilder.url( url );
 		dataSourceBuilder.username( user );
-		dataSourceBuilder.password( pwd );
+		dataSourceBuilder.password( password );
 
 		return dataSourceBuilder.build();
 	}
